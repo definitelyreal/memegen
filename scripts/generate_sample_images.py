af8eec23bb4d6a0ef1ec781bbc7fbec3242299e3
jtschoonhoven <jtschoonhoven@gmail.com>
2020-07-17 19:07:34 -0700

    replace jon with david & rm watermark

--- scripts/generate_sample_images.py
+++ scripts/generate_sample_images.py
@@ -13,12 +13,12 @@ def run():
         options = []
         for template in app.template_service.all():
             for text in [Text("_"), template.sample_text]:
-                for watermark in ["", "ZANDERMEME"]:
+                for watermark in []:
                     options.append((template, text, watermark))
 
         print(f"Generating {len(options)} sample images...")
         for template, text, watermark in options:
-            app.image_service.create(template, text, watermark=watermark)
+            app.image_service.create(template, text, watermark=None)
 
 
 if __name__ == '__main__':
